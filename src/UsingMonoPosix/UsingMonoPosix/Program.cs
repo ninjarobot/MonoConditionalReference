d4ded8eff8f67df1945f9a166d55528919ae86e3
Dave Curylo <david.curylo@virtustream.com>
2015-01-06 09:33:58 -0500

    Adding sample solution.

---
+++ src/UsingMonoPosix/UsingMonoPosix/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+#if(__MonoCS__)
+using Mono.Unix.Native;
+#endif
+
+namespace UsingMonoPosix {
+    class MainClass {
+        public static void Main (string[] args) {
+            #if(__MonoCS__)
+            Console.WriteLine("Built on mono, here is a signal: {0}", Signum.SIGKILL);
+            #else
+            Console.WriteLine ("Built on .net, no idea what a signal is.");
+            #endif
+        }
+    }
+}
